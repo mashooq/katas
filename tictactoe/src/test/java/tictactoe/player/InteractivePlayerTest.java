c076ae048248de6e87607172b32ceb8f4fa6a6c2
Mashooq Badar <mashooq.badar@gmail.com>
2013-07-26 01:36:42 +0100

    simplified test

--- tictactoe/src/test/java/tictactoe/player/InteractivePlayerTest.java
+++ tictactoe/src/test/java/tictactoe/player/InteractivePlayerTest.java
@@ -3,10 +3,10 @@ package tictactoe.player;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import tictactoe.game.GameBoard;
+import tictactoe.game.LineGenerator;
 import tictactoe.game.Mark;
 import tictactoe.game.Move;
 import tictactoe.ui.Prompt;
@@ -17,25 +17,30 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static tictactoe.game.GameBoard.GRID_SIZE;
 import static tictactoe.game.Mark.X;
 import static tictactoe.game.Move.move;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InteractivePlayerTest {
-    @Mock GameBoard gameBoard;
+    GameBoard gameBoard;
     @Mock Player opposition;
     @Mock Prompt prompt;
     InteractivePlayer player;
+    Mark[][] currentGrid = new Mark[GRID_SIZE][GRID_SIZE];
+    Move actualMove;
 
     @Before
     public void setupPlayer() {
         player = new InteractivePlayer(X, prompt);
+        gameBoard = new GameBoard(new LineGenerator()) {
+            public Mark[][] cloneCurrentGrid() { return currentGrid; }
+            public void make(Move move) { actualMove = move; }
+        };
     }
 
     @Test
     public void displaysTheCurrentStateOfTheBoardAndPromptsForNextMove() {
-        Mark[][] currentGrid = new Mark[3][3];
-        given(gameBoard.cloneCurrentGrid()).willReturn(currentGrid);
         given(prompt.readMove()).willReturn(3);
 
         player.makeMove(gameBoard);
@@ -45,15 +50,11 @@ public class InteractivePlayerTest {
 
     @Test
     public void takesMoveFromUsersInput() throws IOException {
-        Mark[][] currentGrid = new Mark[3][3];
-        given(gameBoard.cloneCurrentGrid()).willReturn(currentGrid);
         given(prompt.readMove()).willReturn(3);
 
         player.makeMove(gameBoard);
 
-        ArgumentCaptor<Move> argumentCaptor = ArgumentCaptor.forClass(Move.class);
-        verify(gameBoard).make(argumentCaptor.capture());
-        assertThat(argumentCaptor.getValue(), is(move(X, 0, 2)));
+        assertThat(actualMove, is(move(X, 0, 2)));
     }
 
     @Test
@@ -63,8 +64,6 @@ public class InteractivePlayerTest {
         player.makeMove(gameBoard);
 
         verify(prompt).tryAgain();
-        ArgumentCaptor<Move> argumentCaptor = ArgumentCaptor.forClass(Move.class);
-        verify(gameBoard).make(argumentCaptor.capture());
-        assertThat(argumentCaptor.getValue(), is(move(X, 0, 2)));
+        assertThat(actualMove, is(move(X, 0, 2)));
     }
 }
