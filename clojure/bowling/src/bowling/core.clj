9c88a745b999a79cb8d14867f0ef8bf2301c61e9
Mashooq Badar <mashooq.badar@gmail.com>
2016-05-06 23:22:52 +0100

    removed some duplication

--- clojure/bowling/src/bowling/core.clj
+++ clojure/bowling/src/bowling/core.clj
@@ -1,17 +1,21 @@
 (ns bowling.core)
 
-(defn score-for [s] 
+(defn- spare?[s] (= \/ s))
+(defn- strike? [s] (= \X s))
+(defn- spare-or-strike? [s] (or (spare? s) (strike? s)))
+(defn- miss? [s] (or (= nil s) (= \- s)))
+
+(defn- score-for [s] 
   (cond 
-     (= \X s) 10
-     (or (= nil s) (= \- s))  0 
-     (= \/ s) 10 
+     (spare-or-strike? s)  10
+     (miss? s)  0 
      :else (read-string (str s))))
 
 (defn- score-roll [this-roll rem-rolls]
   (cond 
-    (= \X this-roll) (+ 10 (score-for (first rem-rolls)) (score-for (first (rest rem-rolls))))
-    (= \/ this-roll) (+ 10 (score-for (first rem-rolls)))
-    (= \/ (first rem-rolls)) 0
+    (strike? this-roll) (+ 10 (score-for (first rem-rolls)) (score-for (first (rest rem-rolls))))
+    (spare? this-roll) (+ 10 (score-for (first rem-rolls)))
+    (spare? (first rem-rolls)) 0
     :else (score-for this-roll)))
 
 (defn- score-rolls [acc rolls]
@@ -21,7 +25,7 @@
     acc))
 
 (defn- expand-strikes [rolls]
-  (seq (reduce str  (map #(if  (= \X %) "X-"  (str %)) (seq rolls)))))
+  (seq (reduce str  (map #(if  (strike? %) "X-"  (str %)) (seq rolls)))))
 
 (defn- deduct-extra-rolls [score rolls]
   (- score  (score-rolls 0 (drop 20 (expand-strikes rolls)))))
