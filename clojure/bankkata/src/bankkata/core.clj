a34dc99d9ed9b70a27df689d3d1a51f3c7e3dad7
Mashooq Badar <mashooq.badar@gmail.com>
2016-09-30 13:54:41 +0100

    bankkata: refactored

--- clojure/bankkata/src/bankkata/core.clj
+++ clojure/bankkata/src/bankkata/core.clj
@@ -1,60 +1,57 @@
 (ns bankkata.core
-  (:require [java-time :as jt]))
-
-(defprotocol Account
-    (reset [acc])
-    (add [acc txn])
-    (txns [acc]))
-
-(extend-type clojure.lang.PersistentVector
-  Account
-  (reset [acc] [])
-  (add [acc txn] (conj acc txn))
-  (txns [acc] acc))
-
-(def account (atom []))
-
-(defn new-account [] (swap! account reset))
-(defn local-date [] (jt/local-date))
-
-(defn- add-txn [type amount]
-  (let [date (local-date)]
-    (swap! account add {:type type :date date :amount amount}))
-  nil)
-
-(defn deposit [amount] (add-txn :credit amount))
-(defn withdrawl [amount] (add-txn :debit amount))
-
-
+  (:require [java-time :as jt])
+  (:refer-clojure :exclude [print]))
 
 (defn print-to-console [] #(println %))
+(defn local-date [] (jt/local-date))
 
-(defn- format-amount [txn]
-  (case (:type txn)
-    :credit (format "%.2f" (:amount txn))
-    :debit (str "-" (format "%.2f" (:amount txn)))))
+(defprotocol TransactionStore
+  (add [ts txn])
+  (txns [ts]))
+(extend-type clojure.lang.PersistentVector
+  TransactionStore 
+  (add [ts txn] (conj ts txn))
+  (txns [ts] ts))
 
-(defn- format-transaction [txn balance]
-  (str
-   (jt/format "dd/MM/yyyy" (:date txn)) " | "
-   (format-amount txn) " | "
-   (format "%.2f" balance)))
 
-(defn- new-balance [balance txn]
-  (case (:type txn)
-    :credit (+ balance (:amount txn))
-    :debit (- balance (:amount txn))))
+(defprotocol StatementPrinter
+  (print-txns [sp ts]))
+(defn statement-printer [printfn]
+  (letfn [(format-amount [txn]
+            (case (:type txn)
+              :credit (format "%.2f" (:amount txn))
+              :debit (str "-" (format "%.2f" (:amount txn)))))
 
-(defn- format-transactions [txns balance]
-  (if (seq txns)
+          (format-transaction [txn balance]
+            (str
+             (jt/format "dd/MM/yyyy" (:date txn)) " | "
+             (format-amount txn) " | "
+             (format "%.2f" balance)))
 
-    (let [txn (first txns)
-         new-balance (new-balance balance txn)]
-      (conj (format-transactions (rest txns) new-balance)
-            (format-transaction txn new-balance)))
-    []))
+          (new-balance [balance txn]
+            (case (:type txn)
+              :credit (+ balance (:amount txn))
+              :debit (- balance (:amount txn))))
 
-(defn print-statement []
-  ((print-to-console)
-   (cons  "DATE       | AMOUNT  | BALANCE"
-          (format-transactions (txns @account) 0))))
+          (format-transactions [txns balance]
+            (if (seq txns)
+              (let [txn (first txns)
+                    new-balance (new-balance balance txn)]
+                (conj (format-transactions (rest txns) new-balance)
+                      (format-transaction txn new-balance)))
+              []))
+
+          (print-txns-with-header [txns]
+            (printfn
+             (cons  "DATE       | AMOUNT  | BALANCE"
+                    (format-transactions txns 0))))]
+
+    (reify StatementPrinter
+      (print-txns [sp ts] (print-txns-with-header (txns ts))))))
+
+
+(def account (atom []))
+(defn new-account [] (reset! account []))
+(defn deposit [amount] (swap! account add {:type :credit :date (local-date) :amount amount}) nil)
+(defn withdrawl [amount] (swap! account add {:type :debit :date (local-date) :amount amount}) nil)
+(defn print-statement [] (-> (statement-printer (print-to-console)) (print-txns @account)))
