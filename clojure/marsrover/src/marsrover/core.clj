bb62196a08acc40e69f97b5134ca5c0d021cc497
Mashooq Badar <mashooq.badar@gmail.com>
2017-10-01 18:47:36 +0100

    mars rover - first test

--- clojure/marsrover/src/marsrover/core.clj
+++ clojure/marsrover/src/marsrover/core.clj
@@ -1,64 +1,12 @@
 (ns marsrover.core)
 
-(def position (atom {}))
-(def nav-grid (atom {}))
+(defn- forward [rover]
+  (case (:d rover)
+    :N (assoc rover :y  (+ (:y rover) 1))))
 
-(def turns
-  {:N {:right :E :left :W}
-   :E {:right :S :left :N}
-   :S {:right :W :left :E}
-   :W {:right :N :left :S}})
+(defn- a-move [rover instr]
+  (case instr
+    \M (forward rover)))
 
-(def moves
-  {:backward
-   {:N {:axis :y :op -}
-    :E {:axis :x :op -}
-    :S {:axis :y :op +}
-    :W {:axis :x :op +}}
-
-   :forward
-   {:N {:axis :y :op +}
-    :E {:axis :x :op +}
-    :S {:axis :y :op -}
-    :W {:axis :x :op -}}})
-
-(defn start-position [pos]
-  (reset! position pos))
-
-(defn navigation-grid [grid]
-  (reset! nav-grid grid))
-
-(defn- turn [direction]
-  (swap! position
-         #(assoc % :d (get-in turns [(:d %) direction]))))
-
-(defn- wrap-if-needed [next-pos max]
-  (cond
-    (= next-pos max) 0
-    (> 0 next-pos) (- max 1)
-    :else next-pos))
-
-(defn- next-position [axis op]
-  (let [pos (@position axis)
-        max (@nav-grid axis)
-        next-pos (wrap-if-needed (op pos 1) max)]
-    next-pos))
-
-(defn- obstructed? [axis pos]
-  (let [new-pos (dissoc (assoc @position axis pos) :d)]
-    (some #(= new-pos %) (:obstacles @nav-grid))))
-
-(defn- make-move [direction]
-  (let [{axis :axis op :op} ((moves direction) (:d @position))
-        next-pos (next-position axis op)]
-    (when-not (obstructed? axis next-pos)
-      (swap! position #(assoc % axis next-pos)))))
-
-(defn move [commands]
-  (doseq [command commands]
-    (case command
-      \f (make-move :forward)
-      \b (make-move :backward)
-      \r (turn :right)
-      \l (turn :left)))
-  @position)
+(defn move [rover instructions]
+  (reduce a-move rover (seq instructions)))
--- clojure/marsrover/test/marsrover/core_test.clj
+++ clojure/marsrover/test/marsrover/core_test.clj
@@ -2,44 +2,6 @@
   (:require [clojure.test :refer :all]
             [marsrover.core :refer :all]))
 
-(deftest marsrover
-  (start-position {:x 0 :y 0 :d :N})
-  (navigation-grid {:x 10 :y 10 :obstacles '({:x 5 :y 4})})
-
-  (testing "no moves notifies of current position"
-    (is (= (move '()) {:x 0 :y 0 :d :N})))
-
-  (testing "moves forward steps with from initial position"
-    (is (= (move '(\f))     {:x 0 :y 1 :d :N}))
-    (is (= (move '(\f \f))  {:x 0 :y 3 :d :N})))
-
-  (testing "moves back steps with from initial poistion"
-    (is (= (move '(\b)) {:x 0 :y 2 :d :N}))
-    (is (= (move '(\b \b)) {:x 0 :y 0 :d :N})))
-
-  (testing "wrap around if moves past grid"
-    (is (= (move '(\f \f \f \f \f \f \f \f \f \f)) {:x 0 :y 0 :d :N}))
-    (is (= (move '(\b)) {:x 0 :y 9 :d :N}))
-    (is (= (move '(\f)) {:x 0 :y 0 :d :N})))
-
-  (testing "move after turning right and wrap around grid"
-    (is (= (move '(\r \f)) {:x 1 :y 0 :d :E}))
-    (is (= (move '(\r \f)) {:x 1 :y 9 :d :S}))
-    (is (= (move '(\r \f)) {:x 0 :y 9 :d :W}))
-    (is (= (move '(\r \f)) {:x 0 :y 0 :d :N}))
-
-    (is (= (move '(\r \b)) {:x 9 :y 0 :d :E}))
-    (is (= (move '(\r \b)) {:x 9 :y 1 :d :S}))
-    (is (= (move '(\r \b)) {:x 0 :y 1 :d :W}))
-    (is (= (move '(\r \b)) {:x 0 :y 0 :d :N})))
-
-  (testing "move after turning left"
-    (is (= (move '(\l \f)) {:x 9 :y 0 :d :W}))
-    (is (= (move '(\l \f)) {:x 9 :y 9 :d :S}))
-    (is (= (move '(\l \f)) {:x 0 :y 9 :d :E}))
-    (is (= (move '(\l \f)) {:x 0 :y 0 :d :N})))
-
-  (testing "stopped by obstacles"
-    (is (= (move '(\f \f \f \f \r \f \f \f \f)) {:x 4 :y 4 :d :E}))
-    (is (= (move '(\f)) {:x 4 :y 4 :d :E}))
-    (is (= (move '(\l \f)) {:x 4 :y 5 :d :N}))))
+(deftest mars-rover
+  (testing "moves forward"
+    (is (= (move {:x 0 :y 0 :d :N} "MMM") {:x 0 :y 3 :d :N}))))
