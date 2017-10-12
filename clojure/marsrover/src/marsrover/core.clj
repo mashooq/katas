7f866e7c3a4411a82484d2443737b157268b6a13
Mashooq Badar <mashooq.badar@gmail.com>
2017-10-12 14:43:15 +0100

    mars rover - wrap around a grid

--- clojure/marsrover/src/marsrover/core.clj
+++ clojure/marsrover/src/marsrover/core.clj
@@ -1,15 +1,25 @@
 (ns marsrover.core)
 
-(defn- forward
-  ([rover]
-   (case (:d rover)
-     :N (forward rover :y +)
-     :E (forward rover :x +)
-     :S (forward rover :y -)
-     :W (forward rover :x -)))
+(defn- wrap-if-needed
+  ([grid rover axis]
+   (let [current (rover axis) max (- (grid axis) 1)]
+     (cond
+       (> current max) (assoc rover axis 0)
+       (< current 0) (assoc rover axis max)
+       :else rover)))
+  ([grid rover]
+   (wrap-if-needed grid (wrap-if-needed grid rover :x) :y)))
 
-  ([rover coord op]
-   (assoc rover coord (op (rover coord) 1))))
+(defn- forward
+  ([grid rover]
+   (case (:d rover)
+     :N (forward grid rover :y +)
+     :E (forward grid rover :x +)
+     :S (forward grid rover :y -)
+     :W (forward grid rover :x -)))
+
+  ([grid rover coord op]
+   (wrap-if-needed grid (assoc rover coord (op (rover coord) 1)))))
 
 (defn- turn-right [rover]
   (case (:d rover)
@@ -18,10 +28,11 @@
     :S (assoc rover :d :W)
     :W (assoc rover :d :N)))
 
-(defn- a-move [rover instr]
+(defn- a-move [grid rover instr]
   (case instr
-    \M (forward rover)
+    \M (forward grid rover)
     \R (turn-right rover)))
 
-(defn move [rover instructions]
-  (reduce a-move rover (seq instructions)))
+(defn move [grid rover instructions]
+  (let [a-move-in-grid (partial a-move grid)]
+    (reduce a-move-in-grid rover (seq instructions))))
--- clojure/marsrover/test/marsrover/core_test.clj
+++ clojure/marsrover/test/marsrover/core_test.clj
@@ -2,15 +2,23 @@
   (:require [clojure.test :refer :all]
             [marsrover.core :refer :all]))
 
+(def grid {:x 10 :y 8})
+
 (deftest mars-rover
   (testing "moves forward"
-    (is (= (move {:x 0 :y 0 :d :N} "MMM") {:x 0 :y 3 :d :N}))
-    (is (= (move {:x 0 :y 0 :d :E} "MMM") {:x 3 :y 0 :d :E}))
-    (is (= (move {:x 0 :y 0 :d :S} "MMM") {:x 0 :y -3 :d :S}))
-    (is (= (move {:x 0 :y 0 :d :W} "MMM") {:x -3 :y 0 :d :W})))
+    (is (= (move grid {:x 0 :y 0 :d :N} "MMM") {:x 0 :y 3 :d :N}))
+    (is (= (move grid {:x 0 :y 0 :d :E} "MMM") {:x 3 :y 0 :d :E}))
+    (is (= (move grid {:x 0 :y 5 :d :S} "MMM") {:x 0 :y 2 :d :S}))
+    (is (= (move grid {:x 5 :y 0 :d :W} "MMM") {:x 2 :y 0 :d :W})))
 
   (testing "turns right"
-    (is (= (move {:x 0 :y 0 :d :N} "R") {:x 0 :y 0 :d :E}))
-    (is (= (move {:x 0 :y 0 :d :E} "R") {:x 0 :y 0 :d :S}))
-    (is (= (move {:x 0 :y 0 :d :S} "R") {:x 0 :y 0 :d :W}))
-    (is (= (move {:x 0 :y 0 :d :W} "R") {:x 0 :y 0 :d :N}))))
+    (is (= (move grid {:x 0 :y 0 :d :N} "R") {:x 0 :y 0 :d :E}))
+    (is (= (move grid {:x 0 :y 0 :d :E} "R") {:x 0 :y 0 :d :S}))
+    (is (= (move grid {:x 0 :y 0 :d :S} "R") {:x 0 :y 0 :d :W}))
+    (is (= (move grid {:x 0 :y 0 :d :W} "R") {:x 0 :y 0 :d :N})))
+
+  (testing "wraps around a grid"
+    (is (= (move grid {:x 0 :y 7 :d :N} "M") {:x 0 :y 0 :d :N}))
+    (is (= (move grid {:x 0 :y 0 :d :S} "M") {:x 0 :y 7 :d :S}))
+    (is (= (move grid {:x 9 :y 0 :d :E} "M") {:x 0 :y 0 :d :E}))
+    (is (= (move grid {:x 0 :y 0 :d :W} "M") {:x 9 :y 0 :d :W}))))
