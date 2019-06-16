15ab6480bac816f140040d7fa7abe53db53a16c8
Mashooq Badar <mashooq.badar@gmail.com>
2019-06-16 16:13:09 +0100

    hotel booking kata - added employee policy

--- java/hotel-booking/src/main/java/hotel/HotelAPI.java
+++ java/hotel-booking/src/main/java/hotel/HotelAPI.java
@@ -7,24 +7,22 @@ import java.util.Set;
 
 public class HotelAPI {
     private HashMap<String, String> companyEmployees = new HashMap<>();
-    private HashMap<String, Set<CompanyPolicy>> companyPolicies = new HashMap<>();
+    private HashMap<String, Set<Policy>> companyPolicies = new HashMap<>();
+    private HashMap<String, Set<Policy>> employeePolicies = new HashMap<>();
 
     public boolean book(String hotelId, String employeeId, String roomType) {
+        if (employeePolicies.containsKey(employeeId))
+            return employeePolicies.get(employeeId).contains(new Policy(hotelId, roomType));
+
         String companyId = companyEmployees.get(employeeId);
         if (companyPolicies.containsKey(companyId))
-            return companyPolicies.get(companyId).contains(new CompanyPolicy(hotelId, roomType));
+            return companyPolicies.get(companyId).contains(new Policy(hotelId, roomType));
 
         return false;
     }
 
     public HotelAPI addCompanyPolicy(String companyId, String hotelId, String roomType) {
-        Set<CompanyPolicy> hotels = companyPolicies.get(companyId);
-        if (hotels == null) {
-            hotels = new HashSet<>();
-            companyPolicies.put(companyId, hotels);
-        }
-        hotels.add(new CompanyPolicy(hotelId, roomType));
-        return this;
+        return addPolicy(companyPolicies, companyId, new Policy(hotelId, roomType));
     }
 
     public HotelAPI addCompanyEmployee(String companyId, String employeeId) {
@@ -32,13 +30,22 @@ public class HotelAPI {
         return this;
     }
 
+    public HotelAPI addEmployeePolicy(String employeeId, String hotelId, String roomType) {
+        return addPolicy(employeePolicies, employeeId, new Policy(hotelId, roomType));
+    }
+
+    private HotelAPI addPolicy(HashMap<String, Set<Policy>> policyMap, String key, Policy policy) {
+        Set<Policy> policies = policyMap.computeIfAbsent(key, k -> new HashSet<>());
+        policies.add(policy);
+        return this;
+    }
 }
 
-class CompanyPolicy {
-    final String hotelId;
-    final String roomType;
+class Policy {
+    private final String hotelId;
+    private final String roomType;
 
-    CompanyPolicy(String hotelId, String roomType) {
+    Policy(String hotelId, String roomType) {
         this.hotelId = hotelId;
         this.roomType = roomType;
     }
@@ -47,7 +54,7 @@ class CompanyPolicy {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        CompanyPolicy that = (CompanyPolicy) o;
+        Policy that = (Policy) o;
         return hotelId.equals(that.hotelId) &&
                 roomType.equals(that.roomType);
     }
--- java/hotel-booking/src/test/java/hotel/HotelBookingFacts.java
+++ java/hotel-booking/src/test/java/hotel/HotelBookingFacts.java
@@ -9,28 +9,57 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class HotelBookingFacts {
 
     /*
-    Allow employee to book an available room according to their company policy
-    - not allow when: Room not available
-    - not allow when: company policy doesn't allow
-    - allow when: company policy allows but employee policy does
+    HOTEL BOOKING FACTS:
+    - allow employee to book an available room according to their company policy
+    - not allow when company policy for room type not available
+    - not allow when company policy for hotel not available
+    - not allow when company policy for employee not available
+    - allow when company policy doesnt allow but employee policy does
+    todo: - not allow when company policy allows but employee policy doesnt
+    todo: - only allows when hotel has room available for that type and dates
+    todo: - not allows when hotel has no room available for that type
+    todo: - not allow when hotel has no room of that type available
      */
 
-
     @Test
-    public void allows_booking_for_an_available_room_according_to_the_company_policy() {
+    public void allow_employee_to_book_an_available_room_according_to_their_company_policy() {
         HotelAPI hotelAPI = provided().companyPolicy("company-id", "hotel-id", "room-type")
                 .andCompanyEmployee("company-id", "employee-id").build();
 
         assertThat(hotelAPI.book("hotel-id", "employee-id", "room-type"), is(true));
     }
 
-
     @Test
-    public void does_not_allow_booking_if_room_type_is_not_available() {
+    public void not_allow_when_company_policy_for_room_type_not_available() {
         HotelAPI hotelAPI = provided().companyPolicy("company-id", "hotel-id", "room-type")
                 .andCompanyEmployee("company-id", "employee-id").build();
 
         assertThat(hotelAPI.book("hotel-id", "employee-id", "another-room-type"), is(false));
+    }
+
+    @Test
+    public void not_allow_when_company_policy_for_hotel_not_available() {
+        HotelAPI hotelAPI = provided().companyPolicy("company-id", "hotel-id", "room-type")
+                .andCompanyEmployee("company-id", "employee-id").build();
+
+        assertThat(hotelAPI.book("another-hotel-id", "employee-id", "room-type"), is(false));
+    }
+
+    @Test
+    public void not_allow_when_company_policy_for_employee_not_available() {
+        HotelAPI hotelAPI = provided().companyPolicy("company-id", "hotel-id", "room-type")
+                .andCompanyEmployee("company-id", "employee-id").build();
+
+        assertThat(hotelAPI.book("hotel-id", "another-employee-id", "room-type"), is(false));
+    }
+
+    @Test
+    public void allow_when_company_policy_doesnt_allow_but_employee_policy_does() {
+        HotelAPI hotelAPI = provided().employeePolicy("employee-id", "hotel-id", "room-type")
+                .andCompanyEmployee("company-id", "employee-id").build();
+
+        assertThat(hotelAPI.book("hotel-id", "employee-id", "room-type"), is(true));
+
     }
 
 }
@@ -42,7 +71,7 @@ class HotelAPIBuilder {
         return new HotelAPIBuilder();
     }
 
-    public HotelAPIBuilder() {
+    private HotelAPIBuilder() {
         hotelAPI = new HotelAPI();
     }
 
@@ -55,8 +84,13 @@ class HotelAPIBuilder {
         return hotelAPI;
     }
 
-    public HotelAPIBuilder companyPolicy(String companyId, String hotelId, String roomType) {
+    HotelAPIBuilder companyPolicy(String companyId, String hotelId, String roomType) {
         hotelAPI.addCompanyPolicy(companyId, hotelId, roomType);
+        return this;
+    }
+
+    HotelAPIBuilder employeePolicy(String employeeId, String hotelId, String roomType) {
+        hotelAPI.addEmployeePolicy(employeeId, hotelId, roomType);
         return this;
     }
 }
