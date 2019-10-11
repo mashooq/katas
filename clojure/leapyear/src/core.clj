540824b9c6bcbe141874bb72d12f35ad20af8592
Mashooq Badar <mashooq.badar@gmail.com>
2019-10-11 17:16:57 +0100

    leapyear: final refactor

--- clojure/leapyear/src/core.clj
+++ clojure/leapyear/src/core.clj
@@ -1,17 +1,8 @@
 (ns core)
 
-(defn div-by-100? [year]
-  (zero? (mod year 100)))
-
-(defn div-by-400? [year]
-  (zero? (mod year 400)))
-
-(defn div-by-4? [year]
-  (zero? (mod year 4)))
-
 (defn leap-year? [year]
-  (if (< year 100)
-    (div-by-4? year)
-    (if (div-by-100? year)
-      (div-by-400? year)
-      (div-by-4? year))))
+  (let [div-by? (partial #(zero? (mod year %)))]
+
+    (or (div-by? 400)
+        (and (div-by? 4)
+             (not (div-by? 100))))))
