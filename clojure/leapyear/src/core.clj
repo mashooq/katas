b87097f5a676955a156a50dbd6812106a85f89d1
Mashooq Badar <mashooq.badar@gmail.com>
2019-10-11 15:21:45 +0100

    first refactor

--- clojure/leapyear/src/core.clj
+++ clojure/leapyear/src/core.clj
@@ -1,8 +1,17 @@
 (ns core)
 
+(defn div-by-100? [year]
+  (zero? (mod year 100)))
+
+(defn div-by-400? [year]
+  (zero? (mod year 400)))
+
+(defn div-by-4? [year]
+  (zero? (mod year 4)))
+
 (defn leap-year? [year]
   (if (< year 100)
-    (zero? (mod year 4))
-    (if (zero? (mod year 100))
-      (zero? (mod year 400))
-      (zero? (mod year 4)))))
+    (div-by-4? year)
+    (if (div-by-100? year)
+      (div-by-400? year)
+      (div-by-4? year))))
